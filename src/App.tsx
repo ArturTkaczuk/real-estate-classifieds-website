264b01874f45ff188f0676d84bdcc0664e058a97
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-14 12:09:50 +0200

    Lint code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function submit(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => submit(e)}></form>
     </div>
   );
 }
+
+type AppProps = {
+  prop: string;
+};
+
+export const Bpp = ({ prop }: AppProps): JSX.Element => {
+  return <div></div>;
+};
 
 export default App;
