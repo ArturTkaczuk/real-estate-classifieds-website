0bce065e8931dfba80aac2d5adb6fa95b66462b8
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-18 15:59:28 +0200

    Add css module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 // import React from 'react'
 
+import styles from './Header.module.css'
+
 type HeaderProps = {
   // prop: string
 }
 
 export const Header = (/*{ prop }: HeaderProps*/): JSX.Element => {
   return (
-    <div>
+    <div className={styles.header}>
       <h2>Header</h2>
     </div>
   )
