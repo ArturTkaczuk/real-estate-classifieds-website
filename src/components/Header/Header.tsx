c155923e0b0f2acb400a9bd0027bfde77e19699a
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-22 10:28:40 +0200

    Add hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
-import styles from './Header.module.css'
+import { HamburgerButton } from '../UI/HamburgerButton/HamburgerButton'
+import { LogoTitle } from '../UI/LogoTitle/LogoTitle'
+import styles from './Header.module.scss'
 
 type HeaderProps = {
   // prop: string
 }
 
 export const Header = (/*{ prop }: HeaderProps*/): JSX.Element => {
-  return <div className={styles.header}></div>
+  return (
+    <div className={styles.header}>
+      <LogoTitle />
+      <HamburgerButton />
+    </div>
+  )
 }
