22ff146b3b6b261a013f546b7233f2367f5ef0e0
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-23 19:24:43 +0200

    Fix minor style bugs in Header and Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header/Header'
 import { Menu } from './components/Menu/Menu'
+import './App.scss'
 
 function App() {
   return (
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,23 +1,18 @@
 import { Paper, Typography } from '@mui/material'
 import styles from './Menu.module.scss'
-import menu from '../../assets/images/menu.jpg'
 
-type MenuProps = {
-  // prop: string
-}
-
-export const Menu = (/*{ prop }: MenuProps*/): JSX.Element => {
+export const Menu = (): JSX.Element => {
   return (
     <div className={styles.menu}>
-      <Paper
-        className={styles.textField}
-        sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}
-        elevation={4}
-      >
-        <Typography className={styles.text} variant='h3' component='h1'>
-          The best real estate classifieds platform in Poland
-        </Typography>
-      </Paper>
+      <div className={styles.contentWrapper}>
+        <Paper
+          className={styles.textField}
+          sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}
+          elevation={4}
+        >
+          <h1>The best real estate classifieds platform in Poland</h1>
+        </Paper>
+      </div>
     </div>
   )
 }
