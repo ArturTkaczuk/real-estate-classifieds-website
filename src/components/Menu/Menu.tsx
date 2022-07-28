c05940f363906dc2d286710747a862c7a3b8cad7
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-28 16:55:00 +0200

    Refactor Paper in Menu styled component
    I refactored Paper in Menu to PaperWrapper emotion styled component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,27 @@
-import { Container, Paper } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { Container, Paper, Typography, experimental_sx as sx } from '@mui/material'
+
 import { SearchFilter } from '../SearchFilter/SearchFilter'
 import styles from './Menu.module.css'
+
+const PaperWrapper = styled(Paper)(
+  sx({
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    padding: '25px',
+    margin: '24px 24px 0 0',
+    width: { md: '300px' },
+  }),
+)
 
 export const Menu = (): JSX.Element => {
   return (
     <div className={styles.menu}>
       <Container>
-        <Paper
-          component='h1'
-          sx={{
-            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-            padding: '25px',
-            margin: '24px 24px 0 0',
-            fontWeight: '400',
-            fontSize: '3rem',
-            width: { md: '300px' },
-          }}
-          elevation={4}
-        >
-          The best real estate classifieds platform in Poland
-        </Paper>
+        <PaperWrapper elevation={4}>
+          <Typography component='h1' variant='h3'>
+            The best real estate classifieds platform in Poland
+          </Typography>
+        </PaperWrapper>
         <SearchFilter />
       </Container>
     </div>
