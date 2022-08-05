7e6ebe124938b8bba34af235217c96b93ddfbccf
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-08-05 17:12:22 +0200

    Add ThemeColorContext displayName
    This property is only visible in Chrome React Developer Tools extension
    for debugging

--- src/context/ThemeColorContext.ts
+++ src/context/ThemeColorContext.ts
@@ -2,3 +2,6 @@ import { createContext } from 'react'
 import { MUIColorType } from '../types'
 
 export const ThemeColorContext = createContext<MUIColorType>(undefined)
+
+//Only visible in Chrome React Developer Tools extension - for debugging
+ThemeColorContext.displayName = 'ThemeColorContext'
