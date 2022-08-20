8d8a8957d5152c00c2a1991f77edcc3837b84367
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-08-20 20:04:50 +0200

    Add Repeat Password field to SignUp.tsx

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,22 +1,35 @@
-import { Button, Container, Paper, TextField } from '@mui/material'
+import { Box, Button, Container, Paper, TextField } from '@mui/material'
 import { Field, Form, Formik } from 'formik'
-import { object, string } from 'yup'
+import { object, ref, string } from 'yup'
+import { HeadingPaper } from '../../components/styled/HeadingPaper'
 
-const initialValues = {
-  email: '',
-  userName: '',
-  password: '',
+type SignUpValuesType = {
+  email: string
+  userName: string
+  password: string
+  repeatPassword: string
 }
 
 export const SignUp = (): JSX.Element => {
+  const handleSubmit = ({ email, userName, password }: SignUpValuesType) => {
+    console.log({ email, userName, password })
+  }
+
   return (
     <Container>
-      <Paper>
+      <HeadingPaper>Register</HeadingPaper>
+      <Paper sx={{ maxWidth: '600px', padding: '20px' }}>
         <Formik
-          initialValues={initialValues}
+          initialValues={{
+            email: '',
+            userName: '',
+            password: '',
+            repeatPassword: '',
+          }}
           onSubmit={(values, formikHelpers) => {
-            console.log(values)
-            formikHelpers.resetForm()
+            handleSubmit(values)
+            // Reset form on submit
+            // formikHelpers.resetForm()
           }}
           validationSchema={object({
             email: string().required('Email is required').email('Invalid email'),
@@ -24,48 +37,71 @@ export const SignUp = (): JSX.Element => {
               .required('Username is required')
               .min(2, 'Username should be min. 2 characters long'),
             password: string()
-              .required('Email is required')
+              .required('Password is required')
               .min(8, 'Password should be min. 8 characters long'),
+            repeatPassword: string()
+              .required('Repeat password')
+              .oneOf([ref('password'), null], 'Passwords must match'),
           })}
         >
           {({ errors, isValid, touched, dirty }) => (
             <Form>
-              <Field
-                name='email'
-                type='email'
-                as={TextField}
-                variant='outlined'
-                color={'success'}
-                label='Email'
-                error={!!errors.email && !!touched.email}
-                helperText={!!touched.email && errors.email}
-                fullWidth
-              />
-              <Field
-                name='userName'
-                type='userName'
-                as={TextField}
-                variant='outlined'
-                color={'success'}
-                label='Username'
-                error={!!errors.userName && !!touched.userName}
-                helperText={!!touched.userName && errors.userName}
-                fullWidth
-              />
-              <Field
-                name='password'
-                type='password'
-                as={TextField}
-                variant='outlined'
-                color={'success'}
-                label='Password'
-                error={!!errors.password && !!touched.password}
-                helperText={!!touched.password && errors.password}
-                fullWidth
-              />
-              <Button type='submit' disabled={!dirty || !isValid} fullWidth>
-                Sign up
-              </Button>
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                <Field
+                  name='email'
+                  type='email'
+                  as={TextField}
+                  variant='outlined'
+                  color={'success'}
+                  label='Email'
+                  error={!!errors.email && !!touched.email}
+                  helperText={!!touched.email && errors.email}
+                  fullWidth
+                />
+                <Field
+                  name='userName'
+                  type='userName'
+                  as={TextField}
+                  variant='outlined'
+                  color={'success'}
+                  label='Username'
+                  error={!!errors.userName && !!touched.userName}
+                  helperText={!!touched.userName && errors.userName}
+                  fullWidth
+                />
+                <Field
+                  name='password'
+                  type='password'
+                  as={TextField}
+                  variant='outlined'
+                  color={'success'}
+                  label='Password'
+                  error={!!errors.password && !!touched.password}
+                  helperText={!!touched.password && errors.password}
+                  fullWidth
+                />
+                <Field
+                  name='repeatPassword'
+                  type='password'
+                  as={TextField}
+                  variant='outlined'
+                  color={'success'}
+                  label='Repeat password'
+                  error={!!errors.repeatPassword && !!touched.repeatPassword}
+                  helperText={!!touched.repeatPassword && errors.repeatPassword}
+                  fullWidth
+                />
+                <Button
+                  type='submit'
+                  disabled={!dirty || !isValid}
+                  fullWidth
+                  variant='contained'
+                  color={'success'}
+                  sx={{ height: '50px' }}
+                >
+                  Sign up
+                </Button>
+              </Box>
             </Form>
           )}
         </Formik>
